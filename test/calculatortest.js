fcee87440ed98b0c8cb152be59638fe625eb0759
gghosh <ghosh@host-7-109.mv.mozilla.com>
2013-06-26 17:58:18 -0700

    calculator test of buttons improved

--- test/calculatortest.js
+++ test/calculatortest.js
@@ -4,47 +4,47 @@ var launch = require(__dirname + '/../examples/launch');
 suite('launch', function() {
   var client, subject;
 
-  setup(function() {
-
-  });
-
-  test('#launchtest', function() {
     
-    var Apps = require(__dirname + '/../index'),
-    BootWatcher = require(__dirname + '/../lib/bootwatcher'),
-    Host = require('marionette-host-environment'),
-    Marionette = require('marionette-client'),
-    path = require('path');
+  var Apps = require(__dirname + '/../index'),
+  BootWatcher = require(__dirname + '/../lib/bootwatcher'),
+  Host = require('marionette-host-environment'),
+  Marionette = require('marionette-client'),
+  path = require('path');
 
-    var calc_path = path.resolve(__dirname, '../b2g/Contents/MacOS/calculator/app/');
-    /**
-     * Options to setup our b2g instance with.
-     * @type {Object}
-     */
-    var OPTIONS = {
-      settings: {
-        'ftu.manifestURL': null,
-        'lockscreen.enabled': false
-      },
-      packagedApps: {
-        'calculator.gaiamobile.org': calc_path
+  var calc_path = path.resolve(__dirname, '../b2g/Contents/MacOS/calculator/app/');
+  var OPTIONS = {
+    settings: {
+      'ftu.manifestURL': null,
+      'lockscreen.enabled': false
+    },
+    packagedApps: {
+      'calculator.gaiamobile.org': calc_path
+    }
+  };
+  var B2G_PATH = path.resolve(__dirname, '../b2g');
+  var CALENDAR_URL = 'app://calculator.gaiamobile.org';
+  function toClick(client,toclick) {
+    client.findElements('input',function(err,element){
+      for (var i = 0 ; i < element.length ; i++ ) {
+        element[i].getAttribute('value',function(err,val){
+          if (val === toclick)  {
+            this.click(function(){                                                           
+            });
+          }
+        }.bind(element[i]));
       }
-    };
-
-
-    /**
-     * File path to B2G.
-     * @const {string}
-     */
-    var B2G_PATH = path.resolve(__dirname, '../b2g');
-
-
-    /**
-     * Origin URL for the calendar app.
-     * @const {string}
-     */
-    var CALENDAR_URL = 'app://calculator.gaiamobile.org';
-
+    });
+  }
+  function assertDisplay(client,toassert)  {
+    client.findElement('#display', function(err, element) {
+      element.findElement('b',function(err,param){
+        param.getAttribute('innerHTML',function(err,param){
+          assert.equal(param,toassert);
+        });
+      });
+    });
+  }
+  test('#launchtest', function() {
     Host.spawn(B2G_PATH, OPTIONS, function(err, port, childProcess) {
       
       if (process.env.DEBUG) {
@@ -62,114 +62,47 @@ suite('launch', function() {
         BootWatcher.setup(client, function(err, bootwatcher) {
           function onBoot(evt) {
             bootwatcher.removeListener(BootWatcher.EventType.BOOT, onBoot);
-            //demo(client, childProcess);
-            Apps.setup(client, function(err, apps) {
+              //demo(client, childProcess);
+              Apps.setup(client, function(err, apps) {
               // Ask for all of the apps.
-              var req = apps.mgmt.getAll();
-              req.onsuccess = function(evt) {
+                var req = apps.mgmt.getAll();
+                req.onsuccess = function(evt) {
                 // Prove that we got them by logging them!
-                evt.target.result.forEach(function(app) {
-                  console.log(app.origin);
+                  evt.target.result.forEach(function(app) {
+                    console.log(app.origin);
 
-                  // If this is the calendar app, try launching it!
-                  if (app.origin === CALENDAR_URL) {
-                    app.launch();
-                    
-                    client.findElements('iframe[mozapp]', function(err, element) {
+                    // If this is the calendar app, try launching it!
+                    if (app.origin === CALENDAR_URL) {
+                      app.launch();
                       
-                       if (err) {
-                        console.log('error',err);
-                         // handle case where element was not found
-                       }
-                       console.log('element',element.length);
-                       for (var i = 0 ; i < element.length ; i++) {
+                      client.findElements('iframe[mozapp]', function(err, element) {
                         
-                        element[i].getAttribute('src',function(err,param){
+                         if (err) {
+                          console.log('error',err);
+                           // handle case where element was not found
+                         }
+                         console.log('element',element.length);
+                         for (var i = 0 ; i < element.length ; i++) {
                           
-                          if (param.indexOf(CALENDAR_URL)!=-1)  {
-                            client.switchToFrame(this,function(err,param){
-                              client.findElements('input',function(err,element){
-                                assert.equal(element.length,18);
-                                 for (var i = 0 ; i < element.length ; i++ ) {
-                                    element[i].getAttribute('value',function(err,val){
-                                      if (val === '1')  {
-                                        
-                                        this.click(function(){
-                                            
-                                        });
-                                        client.findElement('#display', function(err, element) {
-                                            element.findElement('b',function(err,param){
-                                              param.getAttribute('innerHTML',function(err,param){
-                                                
-                                                assert.equal(param,'1');
-                                                client.findElements('input',function(err,element){
-                                                  for (var i = 0 ; i < element.length ; i++ ) {
-                                                    element[i].getAttribute('value',function(err,val){
-                                                      if (val === '+')  {
-                                                        this.click(function(){
-                                            
-                                                        });
-                                                        client.findElement('#display', function(err, element) {
-                                                          element.findElement('b',function(err,param){
-                                                            param.getAttribute('innerHTML',function(err,param){
-                                                              assert.equal(param,'1');
-                                                              client.findElements('input',function(err,element){
-                                                                for (var i = 0 ; i < element.length ; i++ ) {
-                                                                  element[i].getAttribute('value',function(err,val){
-                                                                    if (val === '1')  {
-                                                                      this.click(function(){
-                                                          
-                                                                      });
-                                                                      client.findElement('#display', function(err, element) {
-                                                                        element.findElement('b',function(err,param){
-                                                                          param.getAttribute('innerHTML',function(err,param){
-                                                                            assert.equal(param,'1');
-                                                                            client.findElements('input',function(err,element){
-                                                                              for (var i = 0 ; i < element.length ; i++ ) {
-                                                                                element[i].getAttribute('value',function(err,val){
-                                                                                  if (val === '=')  {
-                                                                                    this.click(function(){
-                                                                        
-                                                                                    });
-                                                                                    client.findElement('#display', function(err, element) {
-                                                                                      element.findElement('b',function(err,param){
-                                                                                        param.getAttribute('innerHTML',function(err,param){
-                                                                                          assert.equal(param,'2');
-                                                                                        });
-                                                                                      });
-                                                                                    });
-                                                                                  }
-                                                                                }.bind(element[i]));
-                                                                              }
-                                                                            });
-                                                                          });
-                                                                        });
-                                                                      });
-                                                                    }
-                                                                  }.bind(element[i]));
-                                                                }
-                                                              });
-                                                            });
-                                                          });
-                                                        });
-                                                      }
-                                                    }.bind(element[i]));
-                                                  }
-                                                });
+                          element[i].getAttribute('src',function(err,param){
+                            
+                            if (param.indexOf(CALENDAR_URL)!=-1)  {
+                              client.switchToFrame(this,function(err,param){
+                              
 
-                                              });
-                                            });
-                                          });
-                                      }      
-                                    }.bind(element[i]));
-                                 }
+                                toClick(client,'1');
+                                toClick(client,'+');
+                                
+                                toClick(client,'1');
+                                toClick(client,'=');
+                                assertDisplay(client,'2');
                               });
-                            });
-                          }
-                        }.bind(element[i]));
-                       }
-                    });
-                  }
+
+                            }
+                          }.bind(element[i]));
+                         }
+                      });
+                    }
                 });
               };
             });
